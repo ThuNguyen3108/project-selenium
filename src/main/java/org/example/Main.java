d89885e7ef54ac597d9094b6178d24cd1af80c84
thuthianhnguyen <nguyenthianhthu3108.work@gmail.com>
2025-01-18 10:15:07 +0700

    #comment init project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+class Test {
+    public String nameString = "Auto";
+    // variable is use save type of this variable
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        String temp = "Vo Thai An";
+
+        System.out.println("Hello world!");
+
+    }
+}
